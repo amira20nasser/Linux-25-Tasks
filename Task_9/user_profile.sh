0cc7a622f9d5f87b48396d4ea9ea4c596d229bcb
Amira Nasser <amira20nasser@gmail.com>
2025-03-14 20:19:04 +0200

    feat(task6):complete part 1

---
+++ Task_9/user_profile.sh
@@ -0,0 +1,8 @@
+read -p "Enter Your name: " name
+read -p "Enter Your age: " age
+read -p "Enter Your Favorite Programming Language: " language
+echo   "Thank you, $name! Your profile has been created successfully with the following details:
+- Name: $name
+- Age: $age
+- Favorite Programming Language: $language
+"
